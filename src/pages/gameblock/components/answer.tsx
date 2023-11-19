5652f436ffac1438cce71e325122877f46bef954
Ngô Hà Định <aawsnd@gmail.com>
2023-11-19 16:50:49 +0700

    seperate index to host and players

---
+++ src/pages/gameblock/components/answer.tsx
@@ -0,0 +1,32 @@
+// components/Answer.js
+import React from 'react';
+import Image from 'next/image';
+
+interface AnswerProps {
+  correctAnswer: number; // Assuming correctAnswer is a number
+  isCorrect: boolean;
+}
+
+const Answer: React.FC<AnswerProps> = ({ correctAnswer, isCorrect }) => {
+  return (
+    <div>
+      <body className="flex items-center justify-center h-screen">
+        {isCorrect ? (
+          <div className="p-8 flex flex-col items-center justify-center overflow-hidden">
+            <b className="text-xl">Correct</b>
+            <Image src="/icons/Correct.png" width={50} height={50} alt="Icon" />
+            <div className="bg-gray-500 text-white text-center w-full p-2 mt-2">Great Try</div>
+          </div>
+        ) : (
+          <div className="p-8 flex flex-col items-center justify-center overflow-hidden">
+            <b className="text-xl">Incorrect</b>
+            <Image src="/icons/Incorrect.png" width={50} height={50} alt="Icon" />
+            <div className="bg-gray-500 text-white text-center p-2 mt-2 w-full">Great Try</div>
+          </div>
+        )}
+      </body>
+    </div>
+  );
+};
+
+export default Answer;
---
+++ src/pages/gameblock/components/quiz_player.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+interface QuizData {
+  question: string;
+  answers: string[];
+  correctAnswer: number;
+}
+
+interface QuizProps {
+  quizData: QuizData;
+  onAnswerSelect: (selectedAnswer: number | null) => void;
+}
+
+const Quiz: React.FC<QuizProps> = ({ quizData, onAnswerSelect }) => {
+  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+
+  // dummy data for quizData
+
+  // Check if quizData is defined before destructuring its properties
+  if (!quizData) {
+    return null;
+  }
+
+  const { question, answers, correctAnswer } = quizData;
+
+  const handleAnswerClick = (index: number) => {
+    setSelectedAnswer(index);
+    onAnswerSelect(index);
+  };
+
+  return (
+    <div className="absolute bottom-1 border-2 w-screen border-gray-600 p-7 rounded-lg bg-gray-200 quiz-container">
+      <h1 className="ml-4 mt-2">{question}</h1>
+      <ul className="grid grid-cols-2 gap-4 answer-grid">
+        {answers.map((answer, index) => (
+          <li
+            key={index}
+            onClick={() => handleAnswerClick(index)}
+            style={{
+              backgroundColor: index === selectedAnswer ? 'lightblue' : 'white',
+              cursor: 'pointer',
+            }}
+            className="p-6 m-4 bg-blue-900 cursor-pointer border border-gray-300 rounded-md hover:bg-red-300"
+          >
+            {index}. {answer}
+          </li>
+        ))}
+      </ul>
+      {selectedAnswer !== null && selectedAnswer === correctAnswer && (
+        <p>Correct!</p>
+      )}
+    </div>
+  );
+};
+
+export default Quiz;
